73e13463711b5f11cebf378a8bb2991c55f5bb6a
西野 倫也 <nishino@zeku.co.jp>
2022-02-05 16:19:13 +0900

    bootstrapを使えるようにimportmapを変更。そしてついでにrouteをresourceに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users
 end
