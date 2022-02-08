99ebb304c6c38cacdaa238db2acb9b2b71945577
西野 倫也 <nishino@zeku.co.jp>
2022-02-08 14:05:23 +0900

    ヘッダーメニューのデザイン完成

--- config/importmap.rb
+++ config/importmap.rb
@@ -11,5 +11,5 @@ pin 'jquery', to: 'https://code.jquery.com/jquery-3.6.0.min.js', preload: true
 pin 'react', to: 'https://ga.jspm.io/npm:react@17.0.2/index.js'
 pin 'object-assign', to: 'https://ga.jspm.io/npm:object-assign@4.1.1/index.js'
 pin 'scheduler', to: 'https://ga.jspm.io/npm:scheduler@0.20.2/index.js'
-pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js"
-pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.2/lib/index.js"
+pin 'bootstrap', to: 'https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js'
+pin '@popperjs/core', to: 'https://ga.jspm.io/npm:@popperjs/core@2.11.2/lib/index.js'
