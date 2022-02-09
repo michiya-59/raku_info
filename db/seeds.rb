720b18fd3d43dfddcd49bee8cbc59ff3d20a9d8a
西野 倫也 <nishino@zeku.co.jp>
2022-02-09 15:13:24 +0900

    Railsをカテゴリーおよび、メニュに追加した

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@
 
 # カテゴリーはあらかじめ決まっている値を入れる
 Category.create(name: 'Ruby')
+Category.create(name: 'Rails')
 Category.create(name: 'ColdFusion')
 Category.create(name: 'SQL')
 Category.create(name: 'その他')
