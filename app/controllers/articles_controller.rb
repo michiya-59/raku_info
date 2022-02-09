7136229392e381c777b3b6cdb0d040d91747c311
西野 倫也 <nishino@zeku.co.jp>
2022-02-09 15:32:50 +0900

    --auto-correctで一律修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,13 @@
+# frozen_string_literal: true
+
 class ArticlesController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def show
-  end
+  def show; end
 
-  def new
-  end
+  def new; end
 
-  def create
-  end
+  def create; end
 
-  def edit
-  end
+  def edit; end
 end
