5b62d3d91203538909b39a93050702dbfc00e0f7
西野 倫也 <nishino@zeku.co.jp>
2022-02-16 14:23:20 +0900

    Markdownのテンプレート機能を実装完了

--- app/javascript/controllers/help_window.js
+++ app/javascript/controllers/help_window.js
@@ -1,77 +1,40 @@
-$(function(){
-  $(".popup_help").mouseover(function() {
-    var window_w = window.innerWidth;
-    var marginTop = 0;
-    if(window_w <= 540){
-      var marginLeft = -110;
-    }else{
-      var marginLeft = 76;
+function simple_tooltip(target_items, name){
+  $(target_items).each(function(i){
+    $("body").append("<div class='"+name+"' id='"+name+i+"'><p>"+$(this).attr('title')+"</p></div>");
+    var my_tooltip = $("#"+name+i);
+    if($(this).attr("title") != "" && $(this).attr("title") != "undefined" ){
+    $(this).removeAttr("title").mouseover(function(){
+          my_tooltip.css({opacity:1.0, display:"none"}).fadeIn(0);
+    }).mousemove(function(kmouse){
+        var window_w = window.innerWidth;
+        var marginTop = 53;
+        if(window_w <= 540){
+          var marginLeft = -110;
+        }else{
+          var marginLeft = 0;
+        }
+        var size = 10;
+        var speed = 200;
+
+        var offsetTop = $(this).offset().top + marginTop;
+        var offsetLeft = $(this).offset().left + marginLeft;
+
+        my_tooltip.css({
+          "top": offsetTop,
+          "left": offsetLeft,
+          "font-size": size + "px"
+        }).show(speed);
+    }).mouseout(function(){
+        my_tooltip.css({left:"-9999px"});
+    });
     }
-    var size = 10;
-    var padding = 5;
-    var marginBottom = 0;
-    var height = 45;
-    var speed = 130;
-    var popupObj = $(".popup_help_window");
-
-    if (!popupObj.length) {
-      popupObj = $("<p/>").addClass("popup_help_window").appendTo($("body"));
-    }
-
-    popupObj.html($(this).attr("data-message"));
-
-    var offsetTop = $(this).offset().top + marginTop;
-
-    var offsetLeft = $(this).offset().left + marginLeft;
-
-    popupObj.css({
-      "top": offsetTop,
-      "left": offsetLeft,
-      "font-size": size + "px",
-      "padding": padding + "px",
-      "margin": marginBottom,
-      "height": height + "px"
-    }).show(speed);
-
-  }).mouseout(function() {
-    $(".popup_help_window").text("").hide("fast");
   });
+}
+
+window.addEventListener('DOMContentLoaded', function(){
+  simple_tooltip(".template_part button","template_pop");
 });
 
-$(function(){
-  $(".popup_help").mouseover(function() {
-    var window_w = window.innerWidth;
-    var marginTop = 0;
-    if(window_w <= 540){
-      var marginLeft = -110;
-    }else{
-      var marginLeft = 76;
-    }
-    var padding = 5;
-    var marginBottom = 0;
-    var height = 45;
-    var size = 10;
-    var speed = 130;
-    var popupObj = $(".popup_help_window");
-
-    if (!popupObj.length) {
-      popupObj = $("<p/>").addClass("popup_help_window").appendTo($("body"));
-    }
-
-    popupObj.html($(this).attr("data-message"));
-
-    var offsetTop = $(this).offset().top + marginTop;
-    var offsetLeft = $(this).offset().left + marginLeft;
-
-    popupObj.css({
-      "top": offsetTop,
-      "left": offsetLeft,
-      "font-size": size + "px",
-      "padding": padding + "px",
-      "margin": marginBottom,
-      "height": height + "px"
-    }).show(speed);
-  }).mouseout(function() {
-    $(".popup_help_window").text("").hide("fast");
-  });
+window.addEventListener('DOMContentLoaded', function(){
+  simple_tooltip(".template_part2 button","template_pop2");
 });
