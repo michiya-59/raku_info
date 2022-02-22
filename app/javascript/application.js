2cb96a390dffb0b86e158439b49228ad466f82c5
西野 倫也 <nishino@zeku.co.jp>
2022-02-22 12:45:21 +0900

    投稿機能の文字制限等の修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,4 @@ import "bootstrap";
 import "marked";
 import "controllers/article_template";
 import "controllers/help_window";
+import "controllers/text_count_function";
---
+++ app/javascript/controllers/text_count_function.js
@@ -0,0 +1,37 @@
+$(function(){
+  //カウントするフィールドを監視
+  $("#note_explanation").keyup(function(){
+    //現在入力されている文字
+    var text = $(this).val();
+    //正確にカウントするため改行コード削除
+    text = text.replace((new RegExp("\r\n","g")),"");
+    text = text.replace((new RegExp("\n","g")),"");
+    //現在の文字数
+    var count = text.length;
+    //管理者が設定した上限文字数
+    var moji_limit = $(this).attr("limit");
+    //文字数をリアルタイムの表示
+    $("#moji_suu").text(count);
+    //処理分け
+    if(count == 0){
+      //字を消して0文字となった場合。
+      $("#moji_suu").text("0");
+    } else if(count > moji_limit) {
+      //上限文字数を超えたら赤色表示
+      $("#moji_suu").css("color", "red");
+      $('#article_btn').prop('disabled',true);
+      $('#back_btn').prop('disabled',true);
+      $("#article_btn").css("opacity", "0.4");
+      $("#back_btn").css("opacity", "0.4");
+      $("#validates_error").css("display", "flex");
+    } else {
+      //文字数が範囲内なら色を戻す
+      $("#moji_suu").css("color", "");
+      $('#article_btn').prop('disabled',false);
+      $("#article_btn").css("opacity", "1");
+      $('#back_btn').prop('disabled',false);
+      $("#back_btn").css("opacity", "1");
+      $("#validates_error").css("display", "none");
+    }
+  });
+});
