1ddb2acd9ce4a7ff83ff16992ef0a781cfbfa561
Nishino <m.n.tennis.0509@icloud.com>
2022-03-06 20:04:14 +0900

    投稿が自分野ではなかった場合は編集出来ない様に修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
     @user = User.new(users_params)
     if @user.save
       login(@user)
+      flash[:success] = '新規登録が出来ました'
       redirect_to root_path
     else
       render 'new'
